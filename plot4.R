66da32e45d46c7c27a1359b4f1d04fbb274f56f1
zailen <ilenia.zara@gmail.com>
2015-10-12 01:06:46 +0200

    Third submission

--- plot4.R
+++ plot4.R
@@ -16,22 +16,22 @@ png('plot4.png', width=480, height=480)
 par(mfrow = c(2, 2))
 
 # plot 1,1
-plot(hh_subset$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xaxt='n')
+plot(hh_subset$Global_active_power, type="l", ylab="Global Active Power", xaxt='n', xlab="")
 axis(1, at=c(0, 1441, 2881), labels=c("Thu", "Fri", "Sat"))
 
 #plot 1,2
-plot(hh_subset$Voltage, type="l", ylab="Voltage", xaxt='n')
-axis(1, at=c(0, 1441, 2881), labels=c("Thu", "Fri", "Sat"), xlab="datetime")
+plot(hh_subset$Voltage, type="l", ylab="Voltage", xaxt='n', xlab="datetime")
+axis(1, at=c(0, 1441, 2881), labels=c("Thu", "Fri", "Sat"))
 
 # plot 2,1
-plot(hh_subset$Sub_metering_1, type="l", ylab="Energy sub metering", xaxt='n')
+plot(hh_subset$Sub_metering_1, type="l", ylab="Energy sub metering", xaxt='n', xlab="")
 points(hh_subset$Sub_metering_2, col="red", type="l")
 points(hh_subset$Sub_metering_3, col="blue", type="l")
 
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
 
 # plot 2,2
-plot(hh_subset$Global_reactive_power, ylab="Global_reactive_power", type="l", xaxt='n')
-axis(1, at=c(0, 1441, 2881), labels=c("Thu", "Fri", "Sat"), xlab="datetime")
+plot(hh_subset$Global_reactive_power, ylab="Global_reactive_power", type="l", xaxt='n', xlab="datetime")
+axis(1, at=c(0, 1441, 2881), labels=c("Thu", "Fri", "Sat"))
 
 dev.off()
